b33bd722baa042db4ecb58cbac5a1eb9c52ed71b
Sandhya Babu <151674108+Sandhyagito@users.noreply.github.com>
2025-01-07 21:58:23 +0530

    Update variables.tf

--- ec2_instance/variables.tf
+++ ec2_instance/variables.tf
@@ -1,24 +1,23 @@
 variable "ami_value" {
-  description = "AMI ID for EC2 instance"
-  type        = string
+  description = "AMI ID for the EC2 instance"
 }
 
 variable "instance_type_value" {
   description = "EC2 instance type"
-  type        = string
 }
 
 variable "key_name" {
-  description = "EC2 key pair"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "The ID of the existing VPC"
-  type        = string
+  description = "Key pair name"
 }
 
 variable "subnet_id" {
-  description = "The ID of the subnet"
-  type        = string
+  description = "The subnet ID where the EC2 instance will be launched"
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the EC2 instance will be launched"
+}
+
+variable "aws_security_group" {
+  description = "Security group for the EC2 instance"
 }
