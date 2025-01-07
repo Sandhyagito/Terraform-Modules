e1853e94f24a706f54e352cb5634bdfad5323ceb
Sandhya Babu <151674108+Sandhyagito@users.noreply.github.com>
2025-01-08 00:45:02 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -16,8 +16,9 @@ resource "aws_instance" "my-server" {
   security_groups = [var.aws_security_group]  
 
 root_block_device {
-    volume_size = 25   # Size in GB
-    volume_type = "gp2"  # General Purpose SSD (default)
+  volume_size = var.volume_size
+  volume_type = "gp2"   # General Purpose SSD (default)
+}
 
   tags = {
     Name = "my-server"
