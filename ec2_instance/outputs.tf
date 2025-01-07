f7bee85d212c7e88d2d80d467f236c9e6757b287
Sandhya Babu <151674108+Sandhyagito@users.noreply.github.com>
2025-01-07 21:57:42 +0530

    Update outputs.tf

--- ec2_instance/outputs.tf
+++ ec2_instance/outputs.tf
@@ -1,9 +1,10 @@
 output "instance_public_ip" {
-  value = module.ec2_instance.aws_instance.my-server.public_ip
+  value = aws_instance.my-server.public_ip
 }
 
 output "instance_id" {
-  value = module.ec2_instance.aws_instance.my-server.id
+  value = aws_instance.my-server.id
 }
+
 
 
