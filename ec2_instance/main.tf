8a9ba14dae398bcc1a1f99e4c781006da974acbf
Sandhya Babu <151674108+Sandhyagito@users.noreply.github.com>
2025-01-08 00:13:16 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -2,96 +2,15 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_security_group" "Web-SG" {
-  name        = "Web-SG"
-  vpc_id      = var.vpc_id
-  description = "Security group for web access"
-
-  ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 3000
-    to_port = 10000
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 6443
-    to_port = 6443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 25
-    to_port = 25
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 465
-    to_port = 465
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 27017
-    to_port = 27017
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 30000
-    to_port = 32767
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-sg"
-  }
-}
 
 resource "aws_instance" "my-server" {
-  ami             = var.ami_value
-  instance_type   = var.instance_type_value
-  key_name        = var.key_name
-  subnet_id       = var.subnet_id
-  vpc_id          = var.vpc_id
-  security_groups   = [var.aws_security_group]  # Reference the security group directly
+  ami           = var.ami_value
+  instance_type = var.instance_type_value
+  key_name      = var.key_name
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [var.aws_security_group]  # Security group ID passed from root module
 
   tags = {
-    Name = "my-server"
+    Name = "MyServer"
   }
 }
-
