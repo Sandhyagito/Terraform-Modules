208ab25817f4da15789b0435ee786707ffe0cc70
Sandhya Babu <151674108+Sandhyagito@users.noreply.github.com>
2025-01-08 00:28:28 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -2,13 +2,17 @@ provider "aws" {
     region = "ap-south-1"
 }
 
+variable "vpc_id" {
+  description = "The VPC ID to launch the EC2 instance in."
+  type        = string
+}
 
 resource "aws_instance" "my-server" {
   ami           = var.ami_value
   instance_type = var.instance_type_value
   key_name      = var.key_name
   subnet_id     = var.subnet_id  
-
+  vpc_id        = var.vpc_id
   security_groups = [var.aws_security_group]  
 
   tags = {
