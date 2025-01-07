feb6dfa2ec508385dc848819aa4459a3f4240155
Sandhya Babu <151674108+Sandhyagito@users.noreply.github.com>
2025-01-08 00:13:51 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -14,3 +14,12 @@ resource "aws_instance" "my-server" {
     Name = "MyServer"
   }
 }
+
+# Outputs for EC2 instance details
+output "instance_public_ip" {
+  value = aws_instance.my-server.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.my-server.id
+}
