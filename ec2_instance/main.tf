51d6c0878c540a5ef979c3a60ba5a0e486cc12bf
Sandhya Babu <151674108+Sandhyagito@users.noreply.github.com>
2025-01-08 00:17:20 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -7,11 +7,12 @@ resource "aws_instance" "my-server" {
   ami           = var.ami_value
   instance_type = var.instance_type_value
   key_name      = var.key_name
-  subnet_id     = var.subnet_id
-  vpc_security_group_ids = [var.aws_security_group]  # Security group ID passed from root module
+  subnet_id     = var.subnet_id  # Correct usage, no need to specify vpc_id here
+
+  security_groups = [var.aws_security_group]  # Correct security group reference
 
   tags = {
-    Name = "MyServer"
+    Name = "my-server"
   }
 }
 
