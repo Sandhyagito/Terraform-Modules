48e2e91b93aedfaead3bfaee109dfa8ee8107149
Sandhya Babu <151674108+Sandhyagito@users.noreply.github.com>
2025-01-07 23:15:25 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -87,7 +87,7 @@ resource "aws_instance" "my-server" {
   key_name      = var.key_name
   subnet_id     = var.subnet_id
   vpc_id        = var.vpc_id
-  security_groups = var.aws_security_group
+   security_group_ids = [var.aws_security_group]
 
   tags = {
     Name = "my-server"
